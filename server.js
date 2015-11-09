c36ad07eebd9b3c1c15307bf5cd0b1a0fe827164
tixtixti <tktuononen@hotmail.com>
2015-11-09 18:35:12 +0200

    express

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+
+   res.sendfile('./index.html')
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
